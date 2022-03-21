30349993db05050d2d8020b1b22c084c3762dd16
Syed mozammil karim <47852028+mozammilkarim@users.noreply.github.com>
2022-03-21 19:51:00 +0530

    added projects done

--- script.js
+++ script.js
@@ -7,25 +7,25 @@ const menuEducation = document.getElementById('menuEducation');
 const menuPortfolio = document.getElementById('menuPortfolio');
 const menuCerts = document.getElementById('menuCerts');
 const menuSkills = document.getElementById('menuSkills');
-// const menuArray = [menuAbout, menuEducation, menuTrainings, menuPortfolio, menuCerts, menuSkills];
+// adding events for Menu at right-top to show extended menu
 Menu.addEventListener('click', function (e) {
     e.preventDefault();
-    console.log('Menu clicked');
     menuExtended.style.display = 'block'
 
 })
+// closing extended menu by using duplicate menu in the extended menu
 duplicateMenu.addEventListener('click', function (e) {
     e.preventDefault();
-    console.log('Menu clicked Again');
     menuExtended.style.display = 'none'
 })
-// now getting realSection ELements
+// now getting description Section ELements
 const about = document.getElementById('about');
 const skill = document.getElementById('skill');
 const trainings = document.getElementById('trainings');
 const education = document.getElementById('education');
 const portfolio = document.getElementById('portfolio');
 const certs = document.getElementById('certs');
+// making a list for corressponding description sections with their extended menu links
 const sectionArray = [[menuAbout, about],
 [menuEducation, education],
 [menuTrainings, trainings],
@@ -34,9 +34,11 @@ const sectionArray = [[menuAbout, about],
 [menuSkills, skill]];
 // trainings and certs are compiled in the same section
 
+//removing all description sections by default and adding 
+// events to their corresponding links in extended menu to show them when clicked 
 for (let index = 0; index < sectionArray.length; index++) {
     // initially all sections are disabled
-    sectionArray[index][1].style.display='none';
+    sectionArray[index][1].style.display = 'none';
     sectionArray[index][0].addEventListener('click', () => {
         // removing extended menu
         menuExtended.style.display = 'none'
@@ -49,14 +51,81 @@ for (let index = 0; index < sectionArray.length; index++) {
     })
 }
 // only about section is enabled
-sectionArray[0][1].style.display='block';
+sectionArray[0][1].style.display = 'block';
 
 // now making list of projects done for portfolio
-const myGitHub='https://github.com/mozammilkarim';
-const portfolioPro=[
-    {name:'DiscountQueen',img:'./discountQueen.png',category:'Frond End',url:'https://discountqueen.in/'},
-    {name:'Meditation App',imgUrl:'./Meditation App.png',category:'Java Script',url:'https://github.com/mozammilkarim/vanilla-Java-Script-Projects/tree/main/meditation%20app'},
-    {name:'Random Password Generator',img:'./Random Password Generator.png',category:'Java Script',url:'https://github.com/mozammilkarim/vanilla-Java-Script-Projects/tree/main/password%20generator'},
-    {name:'Desktop Assistant(Jarvis)',img:'./Jarvis.png',category:'Python',url:'https://github.com/mozammilkarim/Python/blob/master/desktopAssistant.py'},
-    {name:'Billing Software',img:'./billingSoftware.png',category:'Python',url:'https://github.com/mozammilkarim/Python/blob/master/GUI%20projects/billingSoftwareGUI.py'}
+const portfolioPro = [
+    { name: 'DiscountQueen', imgUrl: './discountIcon.png', category: 'Front End', url: 'https://discountqueen.in/' },
+    { name: 'Meditation App', imgUrl: './javaScript.jpg', category: 'Java Script', url: 'https://github.com/mozammilkarim/vanilla-Java-Script-Projects/tree/main/meditation%20app' },
+    { name: 'Random Pass Generator', imgUrl: './javaScript.jpg', category: 'Java Script', url: 'https://github.com/mozammilkarim/vanilla-Java-Script-Projects/tree/main/password%20generator' },
+    { name: 'Desktop Assistant(Jarvis)', imgUrl: './python.jpg', category: 'Python', url: 'https://github.com/mozammilkarim/Python/blob/master/desktopAssistant.py' },
+    { name: 'Billing Software', imgUrl: './python.jpg', category: 'Python', url: 'https://github.com/mozammilkarim/Python/blob/master/GUI%20projects/billingSoftwareGUI.py' }
 ]
+// grabbing portfolio section elements to change/append it
+const portfolioResult = document.getElementById('portfolioResult');
+const all = document.getElementById('all');
+const javaScript = document.getElementById('javaScript');
+const python = document.getElementById('python2');
+const frontEnd = document.getElementById('frontEnd');
+
+// now adding event listeners to all links/buttons
+// to dynamically present projects done
+function createProjects(portfolioItem) {
+    // adding all projects in portfolioResult node
+    // that are passed by respection category of link clicked ,i.e. options like js,python
+    const element = document.createElement("div");
+    element.className = 'allPortfolio';
+    element.innerHTML = `<a href="${portfolioItem.url}"><img src="${portfolioItem.imgUrl}" alt="${portfolioItem.name}"></a>
+    <h5>${portfolioItem.name}</h5>`
+    portfolioResult.appendChild(element);
+}
+
+// adding events to portfolio option links/buttons
+// to show their category projects
+all.addEventListener('click', (e) => {
+    e.preventDefault();
+    // empty the portfolio result to show only clicked category results
+    portfolioResult.innerHTML = ``;
+    portfolioPro.forEach(portfolioItem => {
+        // adding all projects in portfolioResult node
+        createProjects(portfolioItem);
+    });
+})
+// for java script projects
+javaScript.addEventListener('click', (e) => {
+    e.preventDefault();
+    // empty the portfolio result
+    portfolioResult.innerHTML = ``;
+    portfolioPro.forEach(portfolioItem => {
+
+        if (portfolioItem.category === 'Java Script') {
+            // adding javaScript projects in portfolioResult node
+            createProjects(portfolioItem)
+        }
+    });
+})
+// for Python projects
+python.addEventListener('click', (e) => {
+    e.preventDefault();
+    // empty the portfolio result
+    portfolioResult.innerHTML = ``;
+    portfolioPro.forEach(portfolioItem => {
+        console.log('inside pyhton click2')
+        if (portfolioItem.category === 'Python') {
+            // adding javaScript projects in portfolioResult node
+            createProjects(portfolioItem)
+        }
+    });
+})
+// for Front End projects
+frontEnd.addEventListener('click', (e) => {
+    e.preventDefault();
+    // empty the portfolio result
+    portfolioResult.innerHTML = ``;
+    portfolioPro.forEach(portfolioItem => {
+        if (portfolioItem.category === 'Front End') {
+            // adding javaScript projects in portfolioResult node
+            createProjects(portfolioItem);
+        }
+    });
+})
